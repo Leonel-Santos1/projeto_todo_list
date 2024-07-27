ce9759e51d4fc4ba393f08028f15aafcbcf1a426
Leonel <franciscoleonelfds@gmail.com>
2024-07-27 17:02:27 -0300

    projeto base

---
+++ todo-list/src/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import "./App.css";
+import { Todo } from "./components/Todo";
+import TodoForm from "./components/TodoForm";
+
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "Criar funcionalidades",
+      category: "Trabalho",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      text: "Estudar React",
+      category: "Estudo",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      text: "Fazer compras",
+      category: "Casa",
+      isCompleted: false,
+    },
+  ]);
+
+  const addTodo = (text, category) => {
+    const newTodo = {
+      id: todos.length + 1,
+      text,
+      category,
+      isCompleted: false,
+    };
+    setTodos([...todos, newTodo]);
+  }
+
+  return <div className="app">
+    <h1>Lista de Tarefas</h1>
+    <div className="todo-list">
+      {
+      todos.map(todo => (
+        <Todo key={todo.id} todo={todo} />
+      ))
+      }
+    </div>
+    <TodoForm />
+  </div>;
+}
+
+export default App;
---
+++ todo-list/src/components/Todo.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const Todo = ({todo}) => {
+  return (
+    <div className="todo">
+      <div className="content">
+        <p>{todo.text}</p>
+        <p className="category">({todo.category})</p>
+      </div>
+      <div>
+        <button className="complete">Completar</button>
+        <button className="remove">Deletar</button>
+      </div>
+    </div>
+  );
+};
