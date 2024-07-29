5b483be2b4580a8bc512dbe2c6225de49fae2ca6
Paulianny Moraes <165077453+Pauliannymoraes@users.noreply.github.com>
2024-07-29 10:34:33 -0300

    App.jsx

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -35,12 +35,18 @@ function App() {
     setTodos([...todos, newTodo]);
   }
 
+  const removeTodo = (id) = {
+    const newTodos = [...todos]
+    const filteredTodos = newTodos.filter(todo => todo.id !== id ? todo : null);
+    setTodos(filteredTodos);
+  }
+
   return <div className="app">
     <h1>Lista de Tarefas</h1>
     <div className="todo-list">
       {
       todos.map(todo => (
-        <Todo key={todo.id} todo={todo} />
+        <Todo key={todo.id} todo={todo} removeTodo={removeTodo} />
       ))
       }
     </div>
