add98bd0352188b3fab26e4858047dfe81156d80
Paulianny Moraes <165077453+Pauliannymoraes@users.noreply.github.com>
2024-07-30 17:28:01 -0300

    App.jsx

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import { Todo } from "./components/Todo";
 import TodoForm from "./components/TodoForm";
+import Search from "./components/Search";
 
 function App() {
   const [todos, setTodos] = useState([
@@ -49,6 +50,7 @@ function App() {
   
   return <div className="app">
     <h1>Lista de Tarefas</h1>
+    <Search />
     <div className="todo-list">
       {
       todos.map(todo => (
